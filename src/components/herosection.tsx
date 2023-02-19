959e5905053240ab9cfbaf491b32db3319580d38
Safeer Al Ehsan <safeeralehsan@outlook.com>
2023-02-19 16:30:25 +0600

    add links

--- src/components/herosection.tsx
+++ src/components/herosection.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { motion } from 'framer-motion';
 import WavyText from "./wavytext";
 import { GrFacebookOption, GrInstagram } from 'react-icons/gr'
+import Link from 'next/link';
 
 export default function HeroSection() {
     return(
@@ -15,7 +16,9 @@ export default function HeroSection() {
                 <div className='absolute z-20 flex h-4/5 items-center'>
                     <div className='ml-60 p-10'>
                         <WavyText text="Cafe Dolce" lowerLetterByPixels={60} tailwindclasses='font-serif text-white text-7xl font-semibold my-3 opacity' />
-                        <span className='text-white font-semibold hover:text-gray-300 transition-colors cursor-pointer ml-2 mt-10'>View Project</span>
+                        <Link href="/comingsoon">
+                            <span className='text-white font-semibold hover:text-gray-300 transition-colors cursor-pointer ml-2 mt-10'>View Project</span>
+                        </Link>
                     </div>
                 </div>
                 <div className='absolute bottom-0 z-30 ml-16 mb-8 flex flex-row'>
