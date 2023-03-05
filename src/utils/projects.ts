c9d3b01d36d41a885b2b22431c1409ed384cc9ca
Safeer Al Ehsan <safeeralehsan@outlook.com>
2023-03-05 15:14:54 +0600

    complete project details

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -9,47 +9,27 @@ export type projectDetails = {
 
 export const projects: projectDetails[] = [
     {
-        name: "Project 1",
-        imageURL: "/images/projects/project1.jpg"
-    },
-    {
-        name: "Project 2",
-        imageURL: "/images/projects/project2.jpg"
-    },
-    {
-        name: "Project 3",
-        imageURL: "/images/projects/project3.jpg"
-    },
-    {
-        name: "Project 4",
-        imageURL: "/images/projects/project4.jpg"
+        name: "A Grand Living",
+        imageURL: "/images/projects/a_grand_living_thumbnail.jpg",
+        description: "Our project focused on creating a grand living space with a blend of soft, modern, and classic elements. To achieve this, we designed customized vaulted ceilings that added depth and dimension to the space. The small bar with a cellar created a functional and stylish entertainment area, while the eclectic furnishings provided a unique and personal touch. \n\nWe opted for a warm color palette to create a cozy and inviting atmosphere throughout the space. The powder room was designed to complement the overall design, adding an extra touch of pizazz to the space.\n\nOur team carefully considered every detail, from the lighting fixtures to the hardware, to ensure that each element was functional and visually appealing. The end result is a luxurious and functional living space that showcases our team's expertise in creating grandeur elements while maintaining a soft, modern and classic feel.\n\nOverall, this project exemplifies our commitment to creating unique and personalized living spaces that reflect our clients' individual style and needs. We are proud to have designed this grand living space, and we look forward to continuing to provide exceptional interior design services to our clients.",
+        mainImage: "/images/projects/a_grand_living/main_image.jpg",
+        verticalImage1: "/images/projects/a_grand_living/vertical_image_1.jpg",
+        verticalImage2: "/images/projects/a_grand_living/vertical_image_2.jpg"
     },
     {
         name: "Cafe Dolce",
-        imageURL: "/images/projects/project5.jpg",
+        imageURL: "/images/projects/cafe_dolce_thumbnail.jpg",
         description: "Our goal was to create a space that not only looks stunning, but also provides a comfortable and inviting atmosphere for guests. We achieved this by selecting a muted color palette with accents of gold and marble. The combination of soft lighting and strategically placed artwork creates a warm and inviting ambiance that is perfect for a luxury cafe experience.\n\nThe seating arrangements were carefully selected to ensure that guests can enjoy the space comfortably. The luxurious and comfortable chairs are upholstered in high-quality materials, providing both style and comfort. The use of booths and tables creates a sense of intimacy, perfect for those who want to enjoy a quiet conversation or a private meeting.\n\nWe paid particular attention to the design of the coffee bar, as this is the centerpiece of any cafe. The bar is crafted from the finest materials and features a sleek design that is both functional and aesthetically pleasing. We ensured that the bar is equipped with the latest equipment to guarantee that the coffee served is of the highest quality.\n\nAt WeDesign Studio, we pride ourselves on our ability to create bespoke designs that are tailored to our clients' specific needs. We are thrilled to have been able to create a luxurious and inviting space for Cafe Dolce that will be enjoyed by guests for years to come.",
         mainImage: "/images/projects/cafe_dolce/main_image.jpg",
         verticalImage1: "/images/projects/cafe_dolce/vertical_image_1.jpg",
         verticalImage2: "/images/projects/cafe_dolce/vertical_image_2.jpg",
     },
     {
-        name: "Project 6",
-        imageURL: "/images/projects/project6.jpg"
-    },
-    {
-        name: "Project 7",
-        imageURL: "/images/projects/project7.jpg"
-    },
-    {
-        name: "Project 8",
-        imageURL: "/images/projects/project8.jpg"
-    },
-    {
-        name: "Project 9",
-        imageURL: "/images/projects/project9.jpg"
-    },
-    {
-        name: "Project 19",
-        imageURL: "/images/projects/project9.jpg"
+        name: "Rooftop Bar and Restaurant",
+        imageURL: "/images/projects/rooftop_bar_restaurant_thumbnail.jpg",
+        description: "The rooftop cocktail bar and restaurant design project is a true masterpiece that showcases a perfect blend of geometry, tactile materials, lush greenery, and plush furnishing. The design team has meticulously curated every aspect of the project to create a one-of-a-kind experience for the guests.\n\nThe use of geometric shapes and patterns is prominent in the design, which adds a unique and modern touch to the space. The combination of different textures and materials, such as wood, metal, and stone, creates a sensory experience that enhances the ambiance of the bar and restaurant.\n\nLush greenery is seamlessly integrated throughout the space, adding a natural element that brings life and energy to the rooftop. The carefully selected plants and foliage not only provide a sense of serenity but also create an inviting atmosphere for guests to enjoy their dining and drinking experience.\n\nThe plush furnishing, including comfortable seating and cozy lighting, makes the rooftop cocktail bar and restaurant the perfect place to relax and unwind after a long day. The attention to detail in every aspect of the design ensures that this project will be one to remember for years to come.\n\nOverall, this project showcases the design team's creativity and attention to detail, resulting in a stunning rooftop cocktail bar and restaurant that is both inviting and memorable.",
+        mainImage: "/images/projects/rooftop_bar_restaurant/main_image.jpg",
+        verticalImage1: "/images/projects/rooftop_bar_restaurant/vertical_image_1.jpg",
+        verticalImage2: "/images/projects/rooftop_bar_restaurant/vertical_image_2.jpg",
     },
 ]
